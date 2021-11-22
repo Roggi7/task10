a17e756f55d43626d65c16db23703ce8ba86810c
Igor <roggi.7@mail.ru>
2021-11-22 12:05:28 +0500

    add img

--- filter.py
+++ filter.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import numpy as np
 
-def Create_color (img_list, i, j, gray_sum, area, step):
+
+def create_color(img_list, i, j, gray_sum, area, step):
     for x in range(i, i + area):
         for y in range(j, j + area):
-            for z in range (3):
+            for z in range(3):
                 img_list[x][y][z] = int(gray_sum // step) * step
+
 
 def Create_IMG(img, out, area, step):
     img_list = np.array(Image.open(img))
@@ -16,10 +18,11 @@ def Create_IMG(img, out, area, step):
         j = 0
         while j < width - 9:
             section = img_list[i: area + i, j: area + j]
-            gray_sum = int(np.sum(section) // (area**2)) / 3
-            Create_color(img_list, i, j, gray_sum, area, step)
+            gray_sum = int(np.sum(section) // (area ** 2)) / 3
+            create_color(img_list, i, j, gray_sum, area, step)
             j += 10
         i += 10
     Image.fromarray(img_list).save(out)
+
 
 Create_IMG(input("name:"), input("out_name:"), int(input("area:")), int(input("step:")))
